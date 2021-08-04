195916cbddbd2683b46cf96e02186426427ca0f1
Vitor Hugo Brasiliense da Rosa <53880209+vitor-hbr@users.noreply.github.com>
2021-08-04 15:36:37 -0300

    feat: Completed the challenge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -7,8 +7,20 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  const [cartSize, setCartSize] = useState<Number>(0);
+
+  const getCartSize = () => {
+    if(cart) {
+      setCartSize(cart.length)
+    } else {
+      setCartSize(0);
+    }
+  }
+
+  useEffect(()=> {
+    getCartSize();
+  },[cart])
 
   return (
     <Container>
@@ -20,7 +32,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
@@ -23,28 +23,64 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+       return JSON.parse(storagedCart);
+     }
 
     return [];
   });
 
+  useEffect(() => {
+    if(cart) {
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+    }
+  }, [cart])
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const stock: Stock = (await api.get(`/stock/${productId}`)).data
+
+      let product: undefined | Product;
+
+      for(let i = 0; i < cart.length; i++) {
+        if(cart[i].id === productId) {
+          product = cart[i];
+          break;
+        }
+      }
+
+      if(product) {
+        if(stock.amount > product.amount + 1) {
+          toast.error('Quantidade solicitada fora de estoque');
+        } else {
+          setCart((prevCart) => prevCart.map(product => (product.id === productId ? {...product, amount: 1}: product)));
+        }
+      } else {
+        if(stock.amount > 0) {
+          const res = await api.get(`/products/${productId}`)
+          const newProduct = res.data;
+          setCart((prevCart) => [...prevCart, {...newProduct, amount: 1} as Product])
+        } else {
+          toast.error('Quantidade solicitada fora de estoque');
+        }
+      }
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      setCart((prevCart) => {
+        const t = prevCart.filter((product) => {return product.id !== productId});
+        console.log(productId);
+        console.log(t);
+        return t
+      })
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
@@ -53,9 +89,17 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if(amount <= 0)
+        return
+      
+        const stock: Stock = (await api.get(`/stock/${productId}`)).data
+        if(stock.amount >= amount) {
+          setCart((prevCart) => prevCart.map(product => (product.id === productId ? {...product, amount: amount}: product)));
+        } else {
+          toast.error('Quantidade solicitada fora de estoque');
+      }
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
